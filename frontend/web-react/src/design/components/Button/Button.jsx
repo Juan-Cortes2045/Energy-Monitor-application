6deb09cc4cddaad5d25fe8578af2db30244a2dae
Juan-Cortes2045 <juanestebanp171@gmail.com>
2026-04-12 10:59:29 -0500

    I've applied the token styles to the reusable button

--- frontend/web-react/src/design/components/Button/Button.jsx
+++ frontend/web-react/src/design/components/Button/Button.jsx
@@ -1,14 +1,49 @@
 import styles from "../../css/Button.module.css";
 import colors from "../../tokens/colors";
+import typography from "../../tokens/typography";
+import radius from "../../tokens/radius";
+import spacing from "../../tokens/spacing";
+import shadows from "../../tokens/shadows";
 
 const Button = ({
   children,
   type = "button",
+  icon,
   onClick,
   variant = "primary",
   size = "medium",
   disabled = false,
 }) => {
+  const  sizeStyles ={
+    small: {
+      padding: `${spacing.xs} ${spacing.sm}`,
+      fontSize: typography.sizes.xs,
+    },
+
+    medium: {
+      padding: `${spacing.sm} ${spacing.md}`,
+      fontSize: typography.sizes.sm,
+    },
+
+    large: {
+      padding: `${spacing.md} ${spacing.lg}`,
+      fontSize: typography.sizes.md,
+    },
+  };
+
+  const variantStyles ={
+    primary:{
+      backgroundColor: colors.surface,
+      color: colors.textPrimary,
+      border: `1px solid ${colors.border}`,
+    },
+    google:{
+      backgroundColor: colors.primary,
+      color: colors.textPrimary,
+      border: `1px solid ${colors.border}`,
+    }
+  }
+
   return (
     <button
       type={type}
@@ -21,10 +56,19 @@ const Button = ({
         ${disabled ? styles.disabled : ""}
       `}
       style={{
-        backgroundColor: colors[variant] || colors.primary,
+        backgroundColor: variant === "google"
+        ? colors.surface
+        : colors[variant] || colors.primary,
+        color: variant === "google"
+        ? colors.textPrimary
+        : "white",
+        border: variant === "google"
+        ? `1px solid ${colors.border}`
+        : "none",
       }}
     >
-      {children}
+      {icon && <img src={icon} alt="icon" className={styles.icon} />}
+      <span>{children}</span>
     </button>
   );
 };
