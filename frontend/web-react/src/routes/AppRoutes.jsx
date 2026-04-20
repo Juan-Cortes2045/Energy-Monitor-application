a8cce536d719dbbe7a1999a0ff3a4c356d0fe213
angelgarcia0 <garmiguelangel89@gmail.com>
2026-04-19 22:19:33 -0500

    interface profile desing

--- frontend/web-react/src/routes/AppRoutes.jsx
+++ frontend/web-react/src/routes/AppRoutes.jsx
@@ -4,17 +4,19 @@ import Login from "../features/auth/pages/Login";
 import Register from "../features/auth/pages/Register";
 import VerifyAccount from "../features/auth/pages/VerifyAccount";
 import RecoverPassword from "../features/auth/pages/RecoverPassword";
+import Profile from "../features/auth/pages/Profile";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/home" element={<Home/>} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
         <Route path="/VerifyAccount" element={<VerifyAccount />} />
         <Route path="/recover-password" element={<RecoverPassword />} />
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
