1d8a42a819b9d47e187e3723e9b7c0158db6b012
JuanCortes2045 <juanestebanp171@gmail.com>
2026-06-24 13:02:07 -0500

    fix(auth): Adjustment to the card size on the login and password recovery screens

--- frontend/web-react/src/features/auth/components/RPasswordForm/VRPassword.jsx
+++ frontend/web-react/src/features/auth/components/RPasswordForm/VRPassword.jsx
@@ -34,7 +34,10 @@ const VRPassword = () => {
 
   return (
     <div className={styles.container}>
-      <Card className={styles.card}>
+      <Card className={styles.card}
+      style={{
+        width: "400px"
+      }}>
         <h2 className={styles.title}>Recuperar contraseña</h2>
 
         <p className={styles.description}>
